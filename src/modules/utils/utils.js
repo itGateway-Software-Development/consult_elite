c8b4be2ad0310bd853213d22115b92a5f12ae5d8
itGateway-Software-Development <developer@itgateway.org>
2025-02-24 12:27:50 +0700

    api change

--- src/modules/utils/utils.js
+++ src/modules/utils/utils.js
@@ -1,2 +1,2 @@
-export const api = "http://localhost:8000/api/v1/";
-// export const api = "https://admin.consultelitemm.com/api/v1/";
+// export const api = "http://localhost:8000/api/v1/";
+export const api = "https://admin.consultelitemm.com/api/v1/";
